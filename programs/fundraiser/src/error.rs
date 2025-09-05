d9835d26bb564f2b588aafe93dcd4635a2556c3c
Abhi16016 <abhinavkale919913@gmail.com>
2025-09-05 23:08:03 +0530

    error

--- programs/fundraiser/src/error.rs
+++ programs/fundraiser/src/error.rs
@@ -1,7 +1,31 @@
 use anchor_lang::prelude::*;
 
 #[error_code]
-pub enum ErrorCode {
-    #[msg("Custom error message")]
-    CustomError,
+pub enum FundraiserError {
+    #[msg("Campaign title too long (max 50 characters)")]
+    TitleTooLong,
+    #[msg("Campaign description too long (max 200 characters)")]
+    DescriptionTooLong,
+    #[msg("Target amount must be greater than 0")]
+    InvalidTargetAmount,
+    #[msg("Campaign duration must be beteen 1 and 365 days")]
+    InvalidDuration,
+    #[msg("Campaign has ended")]
+    CampaignEnded,
+    #[msg("Campaign is paused")]
+    CampaignPaused,
+    #[msg("Only campaign creator can perform this action")]
+    UnauthorizedAccess,
+    #[msg("Target amount not reached yet")]
+    TargetNotReached,
+    #[msg("Insufficient funds in campaign")]
+    InsufficientFunds,
+    #[msg("Cannot delete campaign with funds")]
+    CannotDeleteWithFunds,
+    #[msg("Campaign is not paused")]
+    CannotDeletWithFunds,
+    #[msg("Donation amount too small")]
+    DonationTooSmall,
 }
+
+

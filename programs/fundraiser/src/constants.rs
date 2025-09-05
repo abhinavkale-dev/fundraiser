0006ce91c955c848dd73a3c2c2c230e1e5f55db9
Abhi16016 <abhinavkale919913@gmail.com>
2025-09-05 23:07:40 +0530

    constants

--- programs/fundraiser/src/constants.rs
+++ programs/fundraiser/src/constants.rs
@@ -1,4 +1,7 @@
-use anchor_lang::prelude::*;
+pub const CAMPAIGN_SEED: &[u8] = b"campaign";
+pub const DONATION_SEED: &[u8] = b"donation";
+pub const VAULT_SEED: &[u8] = b"vault";
 
-#[constant]
-pub const SEED: &str = "anchor";
+pub const MIN_DONATION: u64 = 100_000;
+pub const MAX_DUARATION_DAYS: u64 = 365;
+pub const MIN_DUARATION_DAYS: u64 = 1;

f8fa5b654d493524ec394ecfe203f6f9239f219c
Abhi16016 <abhinavkale919913@gmail.com>
2025-09-06 23:34:25 +0530

    state

---
+++ programs/fundraiser/src/state/mod.rs
@@ -0,0 +1,9 @@
+pub mod campaign;
+pub mod withdrawal;
+pub mod donation_record;
+pub mod donor_summary;
+
+pub use campaign::*;
+pub use withdrawal::*;
+pub use donation_record::*;
+pub use donor_summary::*;

5736c94508bd0afbfb7482a9d75b897757b6d052
Abhi16016 <abhinavkale919913@gmail.com>
2025-09-08 18:37:08 +0530

    instructions

--- programs/fundraiser/src/instructions/mod.rs
+++ programs/fundraiser/src/instructions/mod.rs
@@ -1,11 +1,13 @@
 pub mod create_campaign;
 pub mod donate;
 pub mod withdraw;
+pub mod withdraw_partial;
 pub mod update_campaign;
 pub mod delete_campaign;
 
 pub use create_campaign::*;
 pub use donate::*;
 pub use withdraw::*;
+pub use withdraw_partial::*;
 pub use update_campaign::*;
 pub use delete_campaign::*;
